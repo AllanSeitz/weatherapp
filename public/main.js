3d0ff36e9d97de2eb22915fb312386ae0fff95a8
Allan <allan@Allans-MBP.localdomain>
2018-09-19 16:43:01 -0400

    classworks

--- public/main.js
+++ public/main.js
@@ -8,44 +8,23 @@
 
 let weather = document.querySelector('.weather-conditions')
 
-class WeatherList {
+class WeatherAPI {
   constructor(parentSelector) {
     this.parent = document.querySelector(parentSelector)
   }
 
-  addCondition(className, text) {
+  addCondition(text1, text) {
     let _newLi = document.createElement('li')
-    _newLi.classList.add(className)
-    _newLi.textContent = text
+    _newLi.textContent = text1 + text
     this.parent.appendChild(_newLi)
   }
-  addOtherCondition(text) {
-    let _newSpan = document.createElement('span')
-    //_newLi.classList.add(className)
-    _newSpan.textContent = text
-    this.parent.appendChild(_newSpan)
-  }
 }
-// const conditions = (humidity, temp, weth, usa) => {
-// let humidityLI = document.createElement('li')
-// humidityLI.textContent = `Humidity: ${humidity}%`
-// weather.appendChild(humidityLI)
-// let tempLI = document.createElement('li')
-// tempLI.textContent = `Temperature: ${temp} F`
-// weather.appendChild(tempLI)
-// let rainLI = document.createElement('li')
-// rainLI.textContent = `Cloud Conditions: ${weth}`
-// weather.appendChild(rainLI)
-// let countryLI = document.createElement('li')
-// countryLI.textContent = `Country: ${usa}`
-// weather.appendChild(countryLI)
-// }
 const main = () => {
   let button = document.querySelector('.search-button')
   let searchInput = document.querySelector('.search')
   button.addEventListener('click', event => {
     let searchInputValue = searchInput.value
-    console.log(searchInputValue)
+
     let url
     if (isNaN(parseInt(searchInputValue))) {
       url = ('http://api.openweathermap.org/data/2.5/weather?q=' + `${searchInputValue}` + '&units=imperial&appid=b1e7918a1ab5e32426948269440f8781')
@@ -53,6 +32,8 @@ const main = () => {
       url = ('http://api.openweathermap.org/data/2.5/weather?zip=' + `${searchInputValue}` + '&units=imperial&appid=b1e7918a1ab5e32426948269440f8781')
     }
     fetch(url)
+
+
       .then(
         response => {
           return response.json()
@@ -61,12 +42,11 @@ const main = () => {
       .then(
         json => {
           weather.textContent = ''
-
-          const weatherList = new WeatherList('.weather-conditions')
-          weatherList.addCondition('humidity', json.main.humidity)
-          weatherList.addCondition('temp', json.main.temp)
-          weatherList.addCondition('description', json.weather[0].description)
-          weatherList.addCondition('country', json.sys.country)
+          const weatherList = new WeatherAPI('.weather-conditions')
+          weatherList.addCondition('Humidity ', json.main.humidity)
+          weatherList.addCondition('Temp ', json.main.temp)
+          weatherList.addCondition('Description ', json.weather[0].description)
+          weatherList.addCondition('Country ', json.sys.country)
           // const otherParent = document.querySelector('.other-bits')
           // addCondition(otherParent, 'humidity', json.main.humidity)
           // addCondition(otherParent, 'temp', json.main.temp)
